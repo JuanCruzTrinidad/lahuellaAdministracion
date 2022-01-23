109dda60d48dde3b50cca8b16307e77429a965ec
Juan Cruz Trinidad <juancruztrinidad97@gmail.com>
2022-01-22 21:49:33 -0300

    Se añadio autenticación con cognito.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import MenuAppBar from "./components/NavBar";
 import { Grid, Paper } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-export default function App() {
+function App({ signOut, user }) {
+  console.log(user)
   return (
     <>
       <MenuAppBar />
@@ -15,9 +21,13 @@ export default function App() {
         component={'main'}
       >
         <Paper elevation={3} sx={{width: '90%', height: '90%', p: 2}}>
+        <h1>Hello {user.attributes.name}</h1>
+      <button onClick={signOut}>Sign out</button>
         <Outlet />
         </Paper>
       </Grid>
     </>
   );
 }
+
+export default withAuthenticator(App);
