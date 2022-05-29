f74b46ff01ace4c947dacd86a067b153b8cfb5d7
Juan Cruz Trinidad <jtrinidad@lanacion.com.ar>
2022-05-29 14:33:19 -0300

    Se añaden chip con cantidad de casos.

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
-import { Button, Stack, Tab, Tabs } from "@mui/material";
+import { Button, Chip, Stack, Tab, Tabs } from "@mui/material";
 import { fetchData } from "../helpers/actions";
 import { useNavigate } from "react-router-dom";
 import { reportStudents } from "../helpers/excel";
@@ -29,9 +29,9 @@ export default function Students() {
   return (
     <div style={{ height: "85%", width: "70%" }}>
       <Tabs value={value} onChange={handleChange} centered indicatorColor="secondary"  textColor="secondary">
-        <Tab value={"Barbara"} label={`Barbara-${rows.length}`} />
-        <Tab value={"Bahiana"} label={`Bahiana-${rows.length}`} />
-        <Tab value={"Jimena"} label={`Jimena-${rows.length}`}/>
+        <Tab value={"Barbara"} label={`Barbara`} />
+        <Tab value={"Bahiana"} label={`Bahiana`} />
+        <Tab value={"Jimena"} label={`Jimena`}/>
       </Tabs>
       <Stack
         sx={{ width: "100%", mb: 1 }}
@@ -40,6 +40,7 @@ export default function Students() {
         alignItems="center"
         spacing={2}
       >
+        <Chip label={`${rows.length} CASOS ASIGNADOS`} sx={{backgroundColor: '#8dd853', color: 'white' }} />
         <Button size="small" onClick={() => reportStudents(rows)} sx={{color: '#8dd853'}}>
           Descargar reporte
         </Button>
